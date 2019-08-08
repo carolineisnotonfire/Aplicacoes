178adc5def269388603fcddd51ec661d53a3ffef
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 17:12:38 -0300

    o

---
+++ WPFAppNew/Data/Editora.cs
@@ -0,0 +1,30 @@
+namespace WPFAppNew.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Editora")]
+    public partial class Editora
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Editora()
+        {
+            Livros = new HashSet<Livros>();
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(200)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Descricao { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Livros> Livros { get; set; }
+    }
+}
---
+++ WPFAppNew/Data/Usuario.cs
@@ -0,0 +1,65 @@
+namespace WPFAppNew.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Usuario")]
+    public partial class Usuario
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Usuario()
+        {
+            Livros = new HashSet<Livros>();
+            Livros1 = new HashSet<Livros>();
+            Locacao = new HashSet<Locacao>();
+            Locacao1 = new HashSet<Locacao>();
+            Locacao2 = new HashSet<Locacao>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Senha { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public int UsuInc { get; set; }
+
+        public int UsuAlt { get; set; }
+
+        public DateTime DatInc { get; set; }
+
+        public DateTime DatAlt { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Livros> Livros { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Livros> Livros1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Locacao> Locacao { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Locacao> Locacao1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Locacao> Locacao2 { get; set; }
+    }
+}
