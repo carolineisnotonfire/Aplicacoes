3721a02cf87f929f5799ccc82e6e9dca6a37cdc6
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-15 15:17:26 -0300

    olha só

---
+++ WEB/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+
+    public class ValuesController : ApiController
+    {
+        Model1 modelLoc = new Model1();
+        // GET api/values
+        public List<Carros> Get()
+        {
+            return modelLoc.Carros.ToList<Carros>();
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return new string[] { "value" }[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
